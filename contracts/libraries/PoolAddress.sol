f96afbe7f773140acf6b6bd3c2bab7b61cc5f85d
Lily Johnson <lillianjanjohnson@gmail.com>
2024-05-03 13:13:29 -0400

    update

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0x355fc4a1aba90154c05b4099f1f2a9d6e5aff38fef3788db08fec32fcc106378;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x7238eac859ac17c57e8e5c09be600a022695d80b3b3f4fe384142fe3b4e56d06;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
